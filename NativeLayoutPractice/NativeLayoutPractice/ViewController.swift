110dd164bf5c7b9b2eddc8a6da23fc43f4ad1d54
Andrey Prez <aw06.1994@yandex.ru>
2022-11-22 18:51:41 +0800

    Views added

--- NativeLayoutPractice/NativeLayoutPractice/ViewController.swift
+++ NativeLayoutPractice/NativeLayoutPractice/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var view1: UIView!
+    var view2: UIView!
+    var view3: UIView!
+    var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func initViews() {
+        self.view1 = UIView()
+        self.view2 = UIView()
+        self.view3 = UIView()
+        
+        view1.translatesAutoresizingMaskIntoConstraints = false
+        view2.translatesAutoresizingMaskIntoConstraints = false
+        view3.translatesAutoresizingMaskIntoConstraints = false
+        
+        view1.backgroundColor = .darkGray
+        view2.backgroundColor = .black
+        view3.backgroundColor = .brown
+        
+        
+        self.view.addSubview(view1)
+        self.view.addSubview(view2)
+        self.view.addSubview(view3)
     }
 
 
