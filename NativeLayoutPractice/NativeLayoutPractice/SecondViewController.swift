5dde25e8ffb9e1c693863909ccabf260ff1b9bd9
Andrey Prez <aw06.1994@yandex.ru>
2022-11-25 17:11:30 +0800

    Second screen layout with tiny constraints

--- NativeLayoutPractice/NativeLayoutPractice/SecondViewController.swift
+++ NativeLayoutPractice/NativeLayoutPractice/SecondViewController.swift
@@ -12,7 +12,7 @@ class SecondViewController: UIViewController {
     
     var view1: UIView!
     var view2: UIView!
-    var imageView: UIView!
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,7 @@ class SecondViewController: UIViewController {
         view1 = TinyView()
         view2 = TinyView()
         imageView = UIImageView()
+        
         configureViews()
         addSubViews()
         constrainSubviews()
@@ -29,13 +30,24 @@ class SecondViewController: UIViewController {
     
     fileprivate func configureViews() {
         view1.backgroundColor = .blue
+        view2.backgroundColor = .red
+        imageView.image = UIImage(named: "guy")
+        imageView.contentMode = .scaleAspectFit
     }
     
     fileprivate func addSubViews() {
         self.view.addSubview(view1)
+        self.view.addSubview(view2)
+        self.view1.addSubview(imageView)
     }
     
     fileprivate func constrainSubviews() {
-        view1.edgesToSuperview()
+        view1.edgesToSuperview(excluding: .bottom)
+        view1.height(0.65 * self.view.frame.height)
+        view2.edgesToSuperview(excluding: .top)
+        view2.topToBottom(of: view1)
+        imageView.height(200)
+        imageView.width(200)
+        imageView.centerInSuperview()
     }
 }
